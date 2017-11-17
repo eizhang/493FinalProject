7ad7118c73c0f484a5098430bd60e6361e7edbd5
Ori Lindner <olindner@umich.edu>
2017-11-17 16:42:31 -0500

    made four buttons

--- scripts/ori.js
+++ scripts/ori.js
@@ -26,3 +26,11 @@ $(document).ready(function() {
 
 
 });
+
+function populate() {
+  console.log("populate!");
+  $('.game-window').html("<input type='image' src='img/startbutton.png' width='200' height='120'/>");
+  $('.game-window').append("<input type='image' src='img/startbutton.png' width='200' height='120' class='b'/><br><br><br><br><br><br>");
+  $('.game-window').append("<input type='image' src='img/startbutton.png' width='200' height='120' class='c'/>");
+  $('.game-window').append("<input type='image' src='img/startbutton.png' width='200' height='120' class='d'/>");
+}
