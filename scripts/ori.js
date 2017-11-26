90815caa164e1dc71aa471dd5298fd47c7de4ba2
Ori Lindner <olindner@umich.edu>
2017-11-26 13:45:52 -0500

    doneish

--- scripts/ori.js
+++ scripts/ori.js
@@ -14,15 +14,15 @@ function question1() {
 	]
 
 		$('.game-window').append('<h2 class="q1" id="q_1">' + question_1 + '</h2>');
-		$('.game-window').append('<button id="a" type="button" onclick="Incorrect()">' + answers_1[0] + '</button>');
-		$('.game-window').append('<button id="b" type="button" onclick="Incorrect()">' + answers_1[1] + '</button>');
-		$('.game-window').append('<button id="c" type="button" onclick="Correct()">' + answers_1[2] + '</button>');
-		$('.game-window').append('<button id="d" type="button" onclick="Incorrect()">' + answers_1[3] + '</button>');
+		$('.game-window').append('<button class="q1" id="a" type="button" onclick="Incorrect()">' + answers_1[0] + '</button>');
+		$('.game-window').append('<button class="q1" id="b" type="button" onclick="Incorrect()">' + answers_1[1] + '</button>');
+		$('.game-window').append('<button class="q1" id="c" type="button" onclick="Correct()">' + answers_1[2] + '</button>');
+		$('.game-window').append('<button class="q1" id="d" type="button" onclick="Incorrect()">' + answers_1[3] + '</button>');
 }
 
 function question2() {
   console.log("question2!");
-  $('.game-window').html("");
+  $('.q1').remove();
   var question_2 = "Questioning the choice of color";
 	var answers_2 = [
 		"I'm colorblind",
@@ -31,19 +31,117 @@ function question2() {
 		"My cat got stuck in the blinds"
 	]
   $('.game-window').append('<h2 class="q2" id="q_2">' + question_2 + '</h2>');
-  $('.game-window').append('<h2 id="mark">?</h2><br><br><br><br>');
-  $('.game-window').append('<button id="e" type="button" onclick="Incorrect()">' + answers_2[0] + '</button>');
-  $('.game-window').append('<button id="f" type="button" onclick="Incorrect()">' + answers_2[1] + '</button>');
-  $('.game-window').append('<button id="g" type="button" onclick="Incorrect()">' + answers_2[2] + '</button>');
-  $('.game-window').append('<button id="h" type="button" onclick="Incorrect()">' + answers_2[3] + '</button>');
+  $('.game-window').append('<h2 class="q2" id="mark">?</h2>');
+  $('.game-window').append('<br class="q2"><br class="q2"><br class="q2"><br class="q2">');
+  $('.game-window').append('<button class="q2" id="e" type="button" onclick="Incorrect()">' + answers_2[0] + '</button>');
+  $('.game-window').append('<button class="q2" id="f" type="button" onclick="Incorrect()">' + answers_2[1] + '</button>');
+  $('.game-window').append('<button class="q2" id="g" type="button" onclick="Incorrect()">' + answers_2[2] + '</button>');
+  $('.game-window').append('<button class="q2" id="h" type="button" onclick="Incorrect()">' + answers_2[3] + '</button>');
+  $('#mark').click(function() {
+    Correct();
+  });
 }
 function question3() {
   console.log("question3!");
-  $('.game-window').html("");
+  $('.q2').remove();
+  var question_3 = "Pick the best team!";
+  $('.game-window').append('<h2 class="q3" id="q_3">' + question_3 + '</h2>');
+  $('.game-window').append('<img class="q3" id="mich" src="img/michigan.png"></img><br class="q3"><br class="q3">');
+  $('.game-window').append('<img class="q3" id="osu" src="img/osu.png"></img><br class="q3"><br class="q3">');
+  $('.game-window').append('<img class="q3" id="minn" src="img/minn.png"></img><br class="q3"><br class="q3">');
+  $('.game-window').append('<img class="q3" id="penn" src="img/penn.png"></img>');
+  let width = $('.game-window').width() - 75;
+
+    function goRight() {
+      $("#mich").animate({
+        left: width
+      }, 2500, function() {
+      });
+        $("#osu").animate({
+        left: width
+      }, 2500, function() {
+      });
+      $("#minn").animate({
+        left: width
+      }, 2500, function() {
+      });
+        $("#penn").animate({
+        left: width
+      }, 2500, function() {
+      });
+     setTimeout(goLeft, 25);
+    }
+    function goLeft() {
+        $("#mich").animate({
+        left: 0
+      }, 2500, function() {
+      });
+        $("#osu").animate({
+        left: 0
+      }, 2500, function() {
+      });
+      $("#minn").animate({
+        left: 0
+      }, 2500, function() {
+      });
+        $("#penn").animate({
+        left: 0
+      }, 2500, function() {
+      });
+      setTimeout(goRight, 25);
+    }
+
+    setTimeout(goRight, 25);
+
+    $('#mich').click(function() {
+      Correct();
+    });
+    $('#osu').click(function() {
+      Incorrect();
+    });
+    $('#penn').click(function() {
+      Incorrect();
+    });
 }
 
 function question4() {
   console.log("question4!");
+  $('.q3').remove();
+  var question_4 = "Click to cram for the EECS 493 exam!";
+  $('.game-window').append('<h2 class="q4" id="q_4">' + question_4 + '</h2>');
+  $('.game-window').append('<button class="q4" id="brainButton" type="button" onclick="Pump()">Study!</button>');
+  $('.game-window').append('<img class="q4" id="bar" src="img/greenbar.png"></img>');
+  $('.game-window').append('<img class="q4" id="brain" src="img/brain1.png"></img><br class="q4"><br class="q4">');
+  $('.game-window').append('<img class="q4" id="widgetHome" src="img/widgethome.png" style="visibility: hidden"></img>');
+  $('.game-window').append('<img class="q4" id="jquery" src="img/jquery.png" style="visibility: hidden"></img>');
+  $('.game-window').append('<img class="q4" id="angular" src="img/angular.png" style="visibility: hidden"></img>');
+
+}
+
+
+function Pump() {
+  let w = $('#bar').width();
+  if (w >= 100) {
+    $('#widgetHome').css("visibility", "visible");
+    $('#brain').attr("src","img/brain2.png");
+  }
+  if (w >= 200) {
+    $('#jquery').css("visibility", "visible");
+    $('#brain').attr("src","img/brain3.png");
+  }
+  if (w >= 300) {
+    $('#angular').css("visibility", "visible");
+    $('#brain').attr("src","img/brain4.png");
+  }
+  if (w >= 350) {
+    win();
+  }
+  $('#bar').css("width", w+10);
+}
+
+function win() {
+  $('.q4').remove();
+  alert("You win!")
 }
 
 function Correct() {
@@ -81,7 +179,5 @@ function populate() {
 }
 
 $(document).ready(function() {
-  $('#mark').click(function() {
-    alert("click!");
-  });
+
 });
